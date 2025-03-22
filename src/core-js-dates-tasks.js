18a162c1217a80b3d910403e72e285beda2a0108
Dramovik <dramovik1@gmail.com>
2025-03-22 22:42:08 +0200

    feat: done formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -155,8 +155,25 @@ function isDateInPeriod(date, { start, end }) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  console.log(date);
+  const newDate = new Date(Date.parse(date));
+  const M = newDate.getMonth() + 1;
+  const D = newDate.getUTCDate();
+  const Y = newDate.getFullYear();
+  let h = newDate.getUTCHours();
+  const m = `${newDate.getMinutes()}`.padStart(2, '0');
+  const s = `${newDate.getSeconds()}`.padStart(2, '0');
+  let a = null;
+  if (h < 12) {
+    a = 'AM';
+  } else {
+    a = 'PM';
+  }
+  if (h > 12) {
+    h -= 12;
+  }
+  return `${M}/${D}/${Y}, ${h}:${m}:${s} ${a}`;
 }
 
 /**
