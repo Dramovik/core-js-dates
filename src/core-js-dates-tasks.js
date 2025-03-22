c69be760c07f2795e8ecb8ca2e3243d4a7975864
Dramovik <dramovik1@gmail.com>
2025-03-22 21:11:09 +0200

    feat: done isDateInPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -135,10 +135,15 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, { start, end }) {
+  if (
+    Date.parse(date) < Date.parse(start) ||
+    Date.parse(date) > Date.parse(end)
+  ) {
+    return false;
+  }
+  return true;
 }
-
 /**
  * Returns the date formatted in 'M/D/YYYY, hh:mm:ss a'.
  *
