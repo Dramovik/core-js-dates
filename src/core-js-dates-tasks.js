e55a1a4fdbaeb33f2d10108d811fa0c344ec3110
Dramovik <dramovik1@gmail.com>
2025-03-23 11:11:12 +0200

    feat: done getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -264,8 +264,9 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const checkMonth = date.getMonth() + 1;
+  return Math.ceil(checkMonth / 3);
 }
 
 /**
